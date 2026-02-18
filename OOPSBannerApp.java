3cf41cb2b9d7aaac2fe85bbeb718e629c651c7df
tulsee-agrawal <tulsee.bargarh@gmail.com>
2026-02-18 15:03:38 +0530

    coded uc4

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,25 +1,17 @@
 class uc3{
 public static void main(String[] args)
 	{
-
-	System.out.println(String.join(" ","  ***   ","  ***   ","******   ","    ****"));
-	System.out.println(String.join(" "," **  ** "," **  ** ","**    ** ","   **"));
-	System.out.println(String.join(" ","**    **","**    **","**     **","  **"));
-	System.out.println(String.join(" ","**    **","**    **","**    ** ","    **"));
-	System.out.println(String.join(" ","**    **","**    **","**   **  ","      **"));
-	System.out.println(String.join(" ","**    **","**    **","******   ","    **"));
-	System.out.println(String.join(" "," **  ** ","**   ** ","**       ","   **"));
-	System.out.println(String.join(" ","  ***   ","  ***   ","**       ","****"));
-
-	System.out.println(String.join("@","  ***   ","  ***   ","******   ","    ****"));
-	System.out.println(String.join("@"," **  ** "," **  ** ","**    ** ","   **"));
-	System.out.println(String.join("@","**    **","**    **","**     **","  **"));
-	System.out.println(String.join("@","**    **","**    **","**    ** ","    **"));
-	System.out.println(String.join("@","**    **","**    **","**   **  ","      **"));
-	System.out.println(String.join("@","**    **","**    **","******   ","    **"));
-	System.out.println(String.join("@"," **  ** ","**   ** ","**       ","   **"));
-	System.out.println(String.join("@","  ***   ","  ***   ","**       ","****"));
-
-	
+	String lines[]=new String[8];
+	lines[0]=String.join(" ","  ***   ","  ***   ","******   ","    ****");
+	lines[1]=String.join(" "," **  ** "," **  ** ","**    ** ","   **");
+	lines[2]=String.join(" ","**    **","**    **","**     **","  **");
+	lines[3]=String.join(" ","**    **","**    **","**    ** ","    **");
+	lines[4]=String.join(" ","**    **","**    **","**   **  ","      **");
+	lines[5]=String.join(" ","**    **","**    **","******   ","    **");
+	lines[6]=String.join(" "," **  ** ","**   ** ","**       ","   **");
+	lines[7]=String.join(" ","  ***   ","  ***   ","**       ","****");
+	for(String l:lines){
+	  System.out.println(l);
+	}
 	}
 }
