e26f81ed4a4450b8a9dcb9103264c4a49706b88b
tulsee-agrawal <tulsee.bargarh@gmail.com>
2026-02-18 16:06:17 +0530

    coded uc5

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,15 @@
 class uc3{
 public static void main(String[] args)
 	{
-	String lines[]=new String[8];
-	lines[0]=String.join(" ","  ***   ","  ***   ","******   ","    ****");
-	lines[1]=String.join(" "," **  ** "," **  ** ","**    ** ","   **");
-	lines[2]=String.join(" ","**    **","**    **","**     **","  **");
-	lines[3]=String.join(" ","**    **","**    **","**    ** ","    **");
-	lines[4]=String.join(" ","**    **","**    **","**   **  ","      **");
-	lines[5]=String.join(" ","**    **","**    **","******   ","    **");
-	lines[6]=String.join(" "," **  ** ","**   ** ","**       ","   **");
-	lines[7]=String.join(" ","  ***   ","  ***   ","**       ","****");
+	String lines[]={
+	String.join(" ","  ***   ","  ***   ","******   ","    ****"),
+	String.join(" "," **  ** "," **  ** ","**    ** ","   **"),
+	String.join(" ","**    **","**    **","**     **","  **"),
+	String.join(" ","**    **","**    **","**    ** ","    **"),
+	String.join(" ","**    **","**    **","**   **  ","      **"),
+	String.join(" ","**    **","**    **","******   ","    **"),
+	String.join(" "," **  ** ","**   ** ","**       ","   **"),
+	String.join(" ","  ***   ","  ***   ","**       ","****")};
 	for(String l:lines){
 	  System.out.println(l);
 	}
